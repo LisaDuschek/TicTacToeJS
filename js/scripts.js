043405e0ac2bfeb3eb399da16ca3fe93e9c3608f
Evan Clough & Lisa Duschek <student@epicodus.com>
2015-09-16 11:02:14 -0700

    Write constructors Board, Game, Spaces.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,31 @@ function Player(mark){
     this.mark = mark;
 };
 
-function Space(x_coordinate,y_coordinate){
+function Space(x_coordinate, y_coordinate){
     this.x_coordinate = x_coordinate;
     this.y_coordinate = y_coordinate;
+};
+
+Space.prototype.markWith = function(mark) {
+    this.markedBy = mark;
+};
+
+function Board(){
+    this.spaces = {
+        space1: new Space(1,1),
+        space2: new Space(1,2),
+        space3: new Space(1,3),
+        space4: new Space(2,1),
+        space5: new Space(2,2),
+        space6: new Space(2,3),
+        space7: new Space(3,1),
+        space8: new Space(3,2),
+        space9: new Space(3,3)
+    };
+};
+
+function Game(){
+    this.board = new Board;
+    this.player1 = new Player("X");
+    this.player2 = new Player("O");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,46 @@
 describe('Player', function() {
     it("returns the player's mark", function() {
-      var testPlayer = new Player("X");
-      expect(testPlayer.mark).to.equal("X");
-   });
+        var testPlayer = new Player("X");
+        expect(testPlayer.mark).to.equal("X");
+    });
 });
 
 describe('Space', function() {
-    it("returns the player's mark", function() {
-      var testSpace = new Space(1,2);
-      expect(testSpace.x_coordinate).to.equal(1);
-   });
+    it("returns the space's x coordinate", function() {
+        var testSpace = new Space(1,2);
+        expect(testSpace.x_coordinate).to.equal(1);
+    });
+
+    it("returns the space's y coordinate", function() {
+        var testSpace = new Space(1,2);
+        expect(testSpace.y_coordinate).to.equal(2);
+    });
+
+    it("lets a player mark a space", function() {
+        var testPlayer = new Player("X");
+        var testSpace = new Space(1,2);
+        testSpace.markWith(testPlayer);
+        expect(testSpace.markedBy).to.equal(testPlayer);
+    });
+});
+
+describe('Board', function(){
+    it("creates nine spaces when initialized", function() {
+        var testBoard = new Board();
+        expect(Object.keys(testBoard.spaces).length).to.equal(9);
+    });
+});
+
+describe('Game', function(){
+    it("creates a board", function() {
+        var testGame = new Game();
+        expect(testGame.board).to.eql(new Board);
+    });
+
+    it("creates two players", function() {
+        var testGame = new Game();
+        expect(testGame.player1.mark).to.equal("X");
+        expect(testGame.player2.mark).to.equal("O");
+    });
+
 });
